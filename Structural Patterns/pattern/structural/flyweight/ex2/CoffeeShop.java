028faf2ea77bcb1688ad887dec09bd9a3902fad5
sahmed10315 <sahmed10315@gmail.com>
2016-02-13 01:30:27 -0600

    Added flyweight pattern second example

--- Structural Patterns/pattern/structural/flyweight/ex2/CoffeeShop.java
+++ Structural Patterns/pattern/structural/flyweight/ex2/CoffeeShop.java
@@ -12,22 +12,4 @@ public class CoffeeShop {
 		return "\ntotal CoffeeFlavour objects made: " + menu.totalCoffeeFlavoursMade();
 	}
 
-	public static void main(String[] args) {
-		CoffeeShop shop = new CoffeeShop();
-
-		shop.takeOrder("Cappuccino");
-		shop.takeOrder("Frappe");
-		shop.takeOrder("Espresso");
-		shop.takeOrder("Frappe");
-		shop.takeOrder("Cappuccino");
-		shop.takeOrder("Frappe");
-		shop.takeOrder("Espresso");
-		shop.takeOrder("Cappuccino");
-		shop.takeOrder("Espresso");
-		shop.takeOrder("Frappe");
-		shop.takeOrder("Cappuccino");
-		shop.takeOrder("Espresso");
-
-		System.out.println(shop.report());
-	}
 }
