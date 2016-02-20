51fcf9fb0b5ca408051645278fdec1869927ee21
sahmed10315 <sahmed10315@gmail.com>
2016-02-19 20:43:37 -0600

    Added state pattern comments

--- Behavioral Patterns/state/TestState.java
+++ Behavioral Patterns/state/TestState.java
@@ -1,7 +1,8 @@
 package state;
 
 /* State pattern can be a cleaner way for an object to change its behavior at runtime without resorting to large monolithic conditional statements .
- * According to GOF it allows an object to alter its behavior when its internal state changes, thus the object will appear to change its class
+ * According to GOF it allows an object to alter its behavior when its internal state changes, thus the object will appear to change its class.
+ * So use state pattern to remove conditional logic duplication, and replace conditional code with polymorphism.
  * */
 public class TestState {
 	public static void main(String args[]) {
