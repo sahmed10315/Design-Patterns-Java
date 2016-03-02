8f7ca5deae6adc4b3905b15fb51e9b34304e1f19
sahmed10315 <sahmed10315@gmail.com>
2016-03-01 23:13:00 -0600

    Did some refactoring

--- Behavioral Patterns/memento/TestMemento.java
+++ Behavioral Patterns/memento/TestMemento.java
@@ -1,6 +1,7 @@
 package memento;
 
-/* Memento pattern without violating encapsulation, captures and externalize an objects internal state so it can be restored to this state later */
+/* Memento pattern without violating encapsulation, captures and externalize an objects internal state so it can be restored to this state later 
+ * Example of memento pattern can be an implementation of a finite state machine*/
 public class TestMemento {
 	public static void main(String[] args) {
 
@@ -9,7 +10,8 @@ public class TestMemento {
 
 		originator.setState("State #1");
 		originator.setState("State #2");
-		// Usually changing the state of originator is done by the caretaker object
+		// Usually changing the state of originator is done by the caretaker
+		// object
 		// but for clearance its being done outside in the main method.
 		careTaker.add(originator.saveStateToMemento());
 
